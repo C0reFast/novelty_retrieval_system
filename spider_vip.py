6eabda8d62a7770a2c6bde55112236ff16dadf3f
C0reFast <topchenfu@qq.com>
2013-12-21 22:51:31 +0800

    add the page content spider

--- spider_vip.py
+++ spider_vip.py
@@ -3,6 +3,7 @@
 
 import re
 from spider import BaseSpider, Request
+from pyquery import PyQuery as pq
 import helpers
 
 
@@ -40,7 +41,46 @@ class VipPaperUrlSpider(BaseSpider):
                           parser=self.page_parser)
 
 
+class VipPaperSpider(BaseSpider):
+    """fetch paper content"""
+    def __init__(self):
+        super(VipPaperSpider, self).__init__()
+        self.PAPER_ID_RE = re.compile('/(\d+)\.html')
+        self.INSERT_SQL = ('insert into PaperContent(id, Title, Author,'
+                           'Abstract, Keywords, Class, DownloadUrl)'
+                           'values(%d, %s, %s, %s, %s, %s, %s)')
+
+    def content_parser(self, url, page_content):
+        """docstring for content_parser"""
+        con = helpers.mssqlconn()
+        p = pq(page_content)
+        paper_id = int(self.PAPER_ID_RE.search(url).group(1))
+        title = p('h1').text() or '-----'
+        author = p('.author a').text() or '-----'
+        abstract = p('.abstrack').text() or '-----'
+        keywords = p('.keywords a').text() or '-----'
+        paper_class = p('#wxClass').attr.value or '-----'
+        download_url = '-----'
+        try:
+            con.execute_non_query(self.INSERT_SQL,
+                                  (paper_id, title, author, abstract,
+                                   keywords, paper_class, download_url))
+            print paper_id
+        except Exception:
+            print 'not', paper_id
+
+
 if __name__ == '__main__':
-    keyword = '%28Keyword_C%3D%28%E6%98%8E%E7%9F%BE%29%2BTitle_C%3D%28%E6%98%8E%E7%9F%BE%29%29'
-    sp = VipPaperUrlSpider(company_id=1, keyword=keyword)
-    sp.crawl([Request(url=sp.SUB_URL % (sp.keyword, 1, sp.keyword), parser=sp.seed_parser)])
+    #keyword = '%28Keyword_C%3D%28%E6%98%8E%E7%9F%BE%29%2BTitle_C%3D%28%E6%98%8E%E7%9F%BE%29%29'
+    #sp = VipPaperUrlSpider(company_id=1, keyword=keyword)
+    #sp.crawl([Request(url=sp.SUB_URL % (sp.keyword, 1, sp.keyword), parser=sp.seed_parser)])
+    vps = VipPaperSpider()
+    con = helpers.pymssqlconn()
+    cur = con.cursor()
+    cur.execute(('select Path from PaperUrl where not exists'
+                 '(select 1  from PaperContent where id=PaperUrl.id)'))
+    reqs = []
+    for row in cur:
+        url = 'http://lib.cqvip.com%s' % row[0]
+        reqs.append(Request(url=url, parser=vps.content_parser))
+    vps.crawl(reqs)
